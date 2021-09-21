645d3807c9a75ba92fa2d313543395d5479dfa63
Joey <joseph.r.young2002@gmail.com>
2021-09-21 13:56:44 -0500

    loginlogout

--- server.js
+++ server.js
@@ -3,6 +3,43 @@
 require('dotenv').config();
 const express = require('express');
 
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const client = jwksClient({
+  jwksUri: 'https://dev-txxoephp.us.auth0.com/.well-known/jwks.json',
+});
+
+const { promisify } = require('util');
+const verify = promisify(jwt.verify);
+
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    if (err) return callback(err);
+
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+async function verifyUser(authorization) {
+  if (!authorization) return null;
+  let token = authorization.split(' ')[1];
+
+  return await verify(token, getKey, {});
+}
+
+app.get('/halo', async (req, res) => {
+    const { authorization } = req.headers;
+  
+    
+    let user = await verifyUser(authorization);
+    if (!user) {
+      res.sendStatus(401);
+      return;
+    }
+
+    
 const mongoose = require('mongoose');
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
