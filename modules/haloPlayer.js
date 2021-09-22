cbda3b3740857a0309affd1fbc82d39a49b2aaa8
ymcla <ymclapp@yahoo.com>
2021-09-22 12:00:46 -0500

    playlist players are working in the console based on hardcoded playlist

--- modules/haloPlayer.js
+++ modules/haloPlayer.js
@@ -5,36 +5,32 @@ module.exports = { getPlayer };
 
 async function getPlayer(request, response) {
     const appid = process.env.HALO_API_KEY;
-    const playlistID = 
+    const playlistID = request.query.id;
 
     try {
         const playlistResults = await axios.get('https://www.haloapi.com/stats/h5/player-leaderboards/csr/46f2fe0c-8478-4593-82a3-bfb01c1cd63f/d34730b9-bb48-4569-bcdf-afba9c358019', {
-            params: {
-                id: 
-            }
+            
+        params: {
+                id: playlistID,
+            },
 
             headers: { 'Ocp-Apim-Subscription-Key': appid },
         });
+        console.log('I have hit the API..........................................!!!!!', playlistResults);
+        
+        let selectedPLaylist = playlistResults.data.Results.map(playlist => new Playlist(playlist));
+        console.log('These are the players for the playlist..............................', selectedPLaylist);
 
-        let activeSeason = seasonsResults.data.find(season => season.isActive);
-        console.log('This is the first active season..............................', activeSeason);
-
-        // console.log('Here is the seasons info.........................................................!!', playlistResults.data);
-
-        // let seasonData = seasonResults.data.season.map(season => new Season(season));
-
-        response.send(activeSeason);
+        response.send(selectedPLaylist);
     }
     catch (err) {
         errorHandler(err, response);
     }
 }
 //update this name
-class Season {
-    constructor(activeSeason) {
-        this.name = seasonData.name;
-        this.description = seasonData.description;
-        this.id = seasonData.id;
-        // this.description = playlistData.playlist[0].description;
+class Playlist {
+    constructor(selectedPLaylist) {
+        this.playerName = selectedPLaylist.Player.Gamertag;
+        this.playerRank = selectedPLaylist.Rank;
     }
 }
--- server.js
+++ server.js
@@ -51,8 +51,11 @@ app.get('/', (request, response) => {
     response.send('You have reached the GAME HIVE')
 })
 
-const playlist = require('./modules/haloSeason.js');
-app.get('/haloSeason', playlist.getSeason);
+const season = require('./modules/haloSeason.js');
+app.get('/haloSeason', season.getSeason);
+
+const player = require('./modules/haloPlayer.js');
+app.get('/haloPlayer', player.getPlayer);
 
 
 app.get('/halo', async (req, res) => {
