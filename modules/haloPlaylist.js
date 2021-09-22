a2c1faeda6cf23dea2d11e01300f9ce719c7cb54
ymcla <ymclapp@yahoo.com>
2021-09-22 10:16:13 -0500

    data is flowing onto the backend route and console

--- modules/haloPlaylist.js
+++ modules/haloPlaylist.js
@@ -1,31 +1,33 @@
 const axios = require('axios');
 const errorHandler = require('../errorHandler');
 
-module.exports = {getPlaylist};
+module.exports = { getPlaylist };
 
 async function getPlaylist(request, response) {
     const appid = process.env.HALO_API_KEY;
 
     try {
-        const playlistResults = await axios.get('https://www.haloapi.com/stats/h5/player-leaderboards/csr/46f2fe0c-8478-4593-82a3-bfb01c1cd63f/d34730b9-bb48-4569-bcdf-afba9c358019', {
-            headers:  {'Ocp-Apim-Subscription-Key': appid},
-    });
+        const seasonsResults = await axios.get('https://www.haloapi.com/metadata/h5/metadata/seasons', {
+            headers: { 'Ocp-Apim-Subscription-Key': appid },
+        });
 
+        let activeSeason = seasonsResults.data.find(season => season.isActive);
+        console.log('This is the first active season..............................', activeSeason);
 
-    console.log('Here is the playlist info.........................................................!!', playlistResults.data.results);
+        // console.log('Here is the seasons info.........................................................!!', playlistResults.data);
 
-    let playlistData = playlistResults.data.results.map(player => new Playlist(playlist));
+        // let seasonData = seasonResults.data.season.map(season => new Season(season));
 
-    response.send(playlistData);
+        response.send(activeSeason);
+    }
+    catch (err) {
+        errorHandler(err, response);
+    }
 }
-catch(err) {
-    errorHandler(err, response);
-}
-}
 
-class Playlist {
-    constructor(playlistData) {
-        this.name = playlistData.playlist;
+class Season {
+    constructor(activeSeason) {
+        this.name = seasonData.name;
         // this.description = playlistData.playlist[0].description;
     }
 }
