e82d90ec4b5cccbfd9910fae685789ce2ed22639
ymcla <ymclapp@yahoo.com>
2021-09-21 19:27:48 -0500

    loaded some dependencies needed for the authorizing and ran the api.  stuck on getting the correct info back and getting the .map isn't a function error

--- modules/haloPlaylist.js
+++ modules/haloPlaylist.js
@@ -7,14 +7,14 @@ async function getPlaylist(request, response) {
     const appid = process.env.HALO_API_KEY;
 
     try {
-        const playlistResults = await axios.get('https://www.haloapi.com/stats/h5/player-leaderboards/csr/46f2fe0c-8478-4593-82a3-bfb01c1cd63f/d34730b9-bb48-4569-bcdf-afba9c358019'), {
-            header:  {'Ocp-Apim-Subscription-Key':  appid},
-        };
+        const playlistResults = await axios.get('https://www.haloapi.com/stats/h5/player-leaderboards/csr/46f2fe0c-8478-4593-82a3-bfb01c1cd63f/d34730b9-bb48-4569-bcdf-afba9c358019', {
+            headers:  {'Ocp-Apim-Subscription-Key': appid},
+    });
 
 
-    console.log('Here is the playlist info.........................................................!!', playlistResults.data.playlists);
+    console.log('Here is the playlist info.........................................................!!', playlistResults.data.results);
 
-    let playlistData = playlistResults.data.playlists.map(playlist => new playlist(playlist));
+    let playlistData = playlistResults.data.results.map(player => new Playlist(playlist));
 
     response.send(playlistData);
 }
@@ -25,7 +25,7 @@ catch(err) {
 
 class Playlist {
     constructor(playlistData) {
-        this.name = playlistData.playlist[0].name;
-        this.description = playlistData.playlist[0].description;
+        this.name = playlistData.playlist;
+        // this.description = playlistData.playlist[0].description;
     }
 }
